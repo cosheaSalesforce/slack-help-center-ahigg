7998b9a08a497a6a81b72cfb6e670069c17b2e84
hbenyosef <hbenyosef@salesforce.com>
2022-08-10 11:27:05 +0300

    handling reactions

--- controllers/listeners/events.js
+++ controllers/listeners/events.js
@@ -1,8 +1,14 @@
 const reactionsHandler = require("../handlers/reactions");
 
 async function init(app) {
+
     app.event('reaction_added', async ({ event, client, context }) => {
-        console.log("catch");
+        console.log("catch added");
+        await reactionsHandler.handleReactionToPost();
+    });
+
+    app.event('reaction_removed', async ({ event, client, context }) => {
+        console.log("catch removed");
         await reactionsHandler.handleReactionToPost();
     });
 
