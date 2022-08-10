4ea8492918be0d77352f9f7af6b8fcb315db8213
Colm O'Shea <coshea@salesforce.com>
2022-08-10 17:43:59 +0100

    Testing route

--- controllers/listeners/routes.js
+++ controllers/listeners/routes.js
@@ -1,4 +1,22 @@
+const express = require("express");
+const handleSlackMessageResponse = require("../handlers/handleSlackMessageResponse");
+
 async function init(app) {
+    receiver.router.use(express.json());
+    receiver.router.use(express.urlencoded({ extended: true }));
+
+    receiver.router.post('/slack-post', async (req, res) => {
+        try {
+            // TO:DO Add auth check
+
+            console.log('coshea : /slack-post');
+
+
+        } catch (error) {
+            res.send(error);
+        }
+    });
+
 
 }
 
