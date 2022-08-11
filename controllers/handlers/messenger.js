a4ae104d0493c9d5f207f3979f318786ef70a9d2
Colm O'Shea <coshea@salesforce.com>
2022-08-11 18:38:17 +0100

    First posting test

--- controllers/handlers/messenger.js
+++ controllers/handlers/messenger.js
@@ -64,6 +64,9 @@ async function postMessages(app, slackPosts) {
     }
 }
 
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
