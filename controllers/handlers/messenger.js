7cb45a19aea4250738fc08c4c7b8a79c49c70b01
Colm O'Shea <coshea@salesforce.com>
2022-08-11 10:22:16 +0100

    Test

--- controllers/handlers/messenger.js
+++ controllers/handlers/messenger.js
@@ -1,5 +1,19 @@
-async function postMessage() {
-    
+
+
+
+
+
+
+async function postMessage(app, channelId, threadId, messageContent, userEmail, isEphermal, showNewCase) {
+   
+}
+
+async function postMessageToChannel() {
+
+}
+
+async function postMessageToThread() {
+
 }
 
 module.exports = {
---
+++ services/slack.service.js
@@ -0,0 +1,13 @@
+const { App } = require('@slack/bolt');
+
+async function getAppInstance() {
+    return new App({
+        signingSecret: process.env.SLACK_SIGNING_SECRET,
+        token: process.env.SLACK_BOT_TOKEN,
+        receiver
+    });
+}
+
+module.exports = {
+    getAppInstance
+}
