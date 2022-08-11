97ce5c2c661a0e4626c7c0388f46ecfb587fbdf8
hbenyosef <hbenyosef@salesforce.com>
2022-08-11 11:56:20 +0300

    handling reactions

--- controllers/handlers/reactions.js
+++ controllers/handlers/reactions.js
@@ -1,7 +1,11 @@
-async function handleReactionToPost() {
+async function handleReactionToMessage(client, userId, reaction, channelId, messageTs) {
+    userInfo  = await app.client.users.profile.get({
+        user: userId
+    })
+    console.log(userInfo);
 
 }
 
 module.exports = {
-    handleReactionToPost
+    handleReactionToMessage
 }
--- controllers/listeners/events.js
+++ controllers/listeners/events.js
@@ -5,12 +5,7 @@ async function init(app) {
     app.event('reaction_added', async ({ event, client, context }) => {
         console.log("catch added");
         console.log(event);
-        await reactionsHandler.handleReactionToPost();
-    });
-
-    app.event('reaction_removed', async ({ event, client, context }) => {
-        console.log("catch removed");
-        await reactionsHandler.handleReactionToPost();
+        await reactionsHandler.handleReactionToMessage(client, event.user, event.reaction, event.item.channel, event.item.ts);
     });
 
     // app.event('message', async ({ event, client, context }) => {
