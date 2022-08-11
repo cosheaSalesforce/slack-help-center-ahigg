ddca960a83f4cffc3424ff375710e879e7772000
Colm O'Shea <coshea@salesforce.com>
2022-08-11 10:36:46 +0100

    Test

--- services/slack.service.js
+++ services/slack.service.js
@@ -3,8 +3,7 @@ const { App } = require('@slack/bolt');
 async function getAppInstance() {
     return new App({
         signingSecret: process.env.SLACK_SIGNING_SECRET,
-        token: process.env.SLACK_BOT_TOKEN,
-        receiver
+        token: process.env.SLACK_BOT_TOKEN
     });
 }
 
