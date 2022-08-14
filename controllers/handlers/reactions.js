e43a1e87e134ade70999e06b8cd9b508e84c86d4
hbenyosef <hbenyosef@salesforce.com>
2022-08-14 09:41:48 +0300

    handling reactions

--- controllers/handlers/reactions.js
+++ controllers/handlers/reactions.js
@@ -4,6 +4,9 @@ const salesforceService = require("../../services/salesforce.service");
 async function handleReactionToMessage(userId, reaction, channelId, messageTs) {
     const userEmail = await slackService.getUserEmailById(userId);
     console.log(userEmail);
+    console.log(reaction);
+    console.log(channelId);
+    console.log(messageTs);
     await salesforceService.updateCaseStatus(userEmail, reaction, channelId, messageTs);
 
 
