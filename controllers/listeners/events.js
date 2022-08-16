57128248e9feda0a07721a4814c8e5ac598f6d94
Itai <ikatz@salesforce.com>
2022-08-16 15:13:41 +0300

    nothing

--- controllers/listeners/events.js
+++ controllers/listeners/events.js
@@ -5,6 +5,7 @@ async function init(app) {
     app.event('reaction_added', async ({ event, client, context }) => {
         //console.log("catch added");
         //console.log(event);
+        //
         await reactionsHandler.handleReactionToMessage(event.user, event.reaction, event.item.channel, event.item.ts);
     });
 
