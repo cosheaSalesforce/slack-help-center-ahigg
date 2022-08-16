5177245d3300ec41cb18a11a6e555efef1661a13
Itai <ikatz@salesforce.com>
2022-08-16 15:26:55 +0300

    fixed email to ID in workflow trigger

--- controllers/handlers/deflectFromWorkflow.js
+++ controllers/handlers/deflectFromWorkflow.js
@@ -1,15 +1,17 @@
 const deflectCaseEphemeralFormat = require("../../slack-ui/blocks/deflectCaseEphemeralFormat");
 const slackService = require("../../services/slack.service");
 
-async function postDeflectionMessage(username, channelId) {
+async function postDeflectionMessage(userEmail, channelId) {
 
     var channelIdSub = channelId.substring(2, channelId.length - 1);
     var block = await deflectCaseEphemeralFormat.createDeflectionFormat(channelIdSub);
     var app = await slackService.getAppInstance();
+    var userId = slackService.getUserIdByEmail(userEmail)
+    console.log(userId);
 
     await app.client.chat.postEphemeral({
         channel: channelIdSub,
-        user: 'U03939T1E8N',
+        user: userId,
         text: "Before you create a case, check out this helpful information!",
         blocks: block,
     });
