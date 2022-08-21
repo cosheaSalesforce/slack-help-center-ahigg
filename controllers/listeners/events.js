4c0bba899b702a218213c0c3206cd41f020ee701
Itai <ikatz@salesforce.com>
2022-08-21 12:10:05 +0300

    testing deflection message from workflow

--- controllers/listeners/events.js
+++ controllers/listeners/events.js
@@ -15,8 +15,8 @@ async function init(app) {
     });
 
     app.event('message', async ({ event, client, context }) => {
-        //console.log("catch message");
-        //console.log(event);
+        console.log("catch message");
+        console.log(event);
     });
 
 }
