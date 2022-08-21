019d1fa9b8a37c8a6c04b87b45178771f0395198
Itai <ikatz@salesforce.com>
2022-08-21 12:52:26 +0300

    testing deflection message from workflow

--- controllers/listeners/messages.js
+++ controllers/listeners/messages.js
@@ -5,9 +5,8 @@ async function init(app) {
 
     app.message(subtype('bot_message'), ({ event, logger }) => {
         console.log('managed to listen to a bot_message event');
-        await ack();
         try {
-            caseCreationHandler.createHcCaseFromSlack(event, message);
+            await caseCreationHandler.createHcCaseFromSlack(event, message);
         } catch (error) {
             console.error(error);
         }
