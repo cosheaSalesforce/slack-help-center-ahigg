f169a5f119e4ca2f6587016977144269e5e156c1
hbenyosef <hbenyosef@salesforce.com>
2022-08-31 13:10:01 +0300

    error handling

--- controllers/handlers/reactions.js
+++ controllers/handlers/reactions.js
@@ -3,18 +3,23 @@ const salesforceService = require("../../services/salesforce.service");
 
 async function handleReactionToMessage(userId, reaction, channelId, messageTs) {
     if (reaction == 'registered' || reaction == 'check') {
-        if(userId == slackService.getBotId()) {
-            console.log(userId);
-            return;
-        }
+        // if(userId == slackService.getBotId()) {
+        //     console.log(userId);
+        //     return;
+        // }
         const statusToUpdate = (reaction == 'registered') ? 'Working' : 'Closed';
         const userEmail = await slackService.getUserEmailById(userId);
+        if(userEmail == null) {
+            return
+        }
         const parentMessageTs = await slackService.getParentMessageTs(channelId, messageTs);
         console.log(parentMessageTs);
         const messageContent = await slackService.getMessageContent(channelId, messageTs);
         const messageOwnerId = await slackService.getMessageOwner(channelId, messageTs);
         const messageOwnerEmail = await slackService.getUserEmailById(messageOwnerId);
-        console.log(messageOwnerEmail);
+        if(messageOwnerEmail == null) {
+            return
+        }
         await salesforceService.updateCaseStatus(userEmail, statusToUpdate, channelId, messageTs, parentMessageTs, messageContent, messageOwnerEmail);
     }
     else {
@@ -24,13 +29,15 @@ async function handleReactionToMessage(userId, reaction, channelId, messageTs) {
 
 async function addReactionToMessage(app, reqBody) {
     console.log('adding a reaction');
-    app.client.reactions.add({
-        channel: reqBody[0].channelId,
-        name: reqBody[0].emoji,
-        timestamp: reqBody[0].messageTs
-    })
-
-    
+    try {
+        app.client.reactions.add({
+            channel: reqBody[0].channelId,
+            name: reqBody[0].emoji,
+            timestamp: reqBody[0].messageTs
+        })
+    } catch(error) {
+        console.error(error);
+    }
 }
 
 module.exports = {
