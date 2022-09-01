2c3d92e6307a1650a2e26a808caad27a6f66049c
hbenyosef <hbenyosef@salesforce.com>
2022-09-01 09:40:34 +0300

    getting bot info

--- services/slack.service.js
+++ services/slack.service.js
@@ -90,7 +90,7 @@ async function getMessageContent(channelId, ts) {
 
 async function getBotId() {
     const app = await getAppInstance();
-    console.log(app.bot);
+    console.log(app.bots);
     // const botInfo = await app.client.bots.info();
     // console.log(app.client);
     // return botInfo.bot.id;
