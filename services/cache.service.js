f7eb1883a4e67d8d681fd5e0b0b1296fe299b90c
Colm O'Shea <coshea@salesforce.com>
2022-09-05 17:16:29 +0100

    Yet another push

--- services/cache.service.js
+++ services/cache.service.js
@@ -1,6 +1,9 @@
 const salesforce = require("./salesforce.service");
 const fs = require('fs');
 
+const redis = require("redis");
+
+
 async function cacheChannelMessages() {
     try {
         console.log('cacheChannelMessages called.');
@@ -15,20 +18,34 @@ async function cacheChannelMessages() {
         console.log('jsonObj: ' , jsonObj);
 
 
-        var location = './json/channelMessages.json';
-        fs.exists(location, function(exists) {
-            if(!exists) {
-                console.log('Error, file does not exist: ' + location);
-                return;
-            }
-            fs.writeFile(location, JSON.stringify(jsonObj), 'utf8', (err, data) => {
-                if(err) {
-                    console.log(err);
-                }
-                console.log('Updated channelMessages.json: ' , data);
-                return;
-            });
-        });
+        // var location = './json/channelMessages.json';
+        // fs.exists(location, function(exists) {
+        //     if(!exists) {
+        //         console.log('Error, file does not exist: ' + location);
+        //         return;
+        //     }
+        //     fs.writeFile(location, JSON.stringify(jsonObj), 'utf8', (err, data) => {
+        //         if(err) {
+        //             console.log(err);
+        //         }
+        //         console.log('Updated channelMessages.json: ' , data);
+        //         return;
+        //     });
+        // });
+
+
+
+
+        console.log('DOING THE REDIS STUFF!');
+        const client = redis.createClient({ url: process.env.REDIS_URL });
+        client.on('error', (err) => console.log('Redis Client Error', err));
+
+        await client.connect();
+        for(var key in jsonObj) {
+            await client.set(key, jsonObj[key]);
+        }
+        console.log('KEYS HAVE BEEN SET');
+
     } catch(ex) {
         console.log(ex);
         return;
