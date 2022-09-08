5cfbb5eb57bd5fe4cffb3592a82f24f0a152c2c0
Itai <ikatz@salesforce.com>
2022-09-08 13:04:01 +0300

    working on final touches

--- controllers/handlers/search.js
+++ controllers/handlers/search.js
@@ -1,23 +1,23 @@
 const salesforceService = require("../../services/salesforce.service");
 const mixpanelService = require("../../services/mixpanel.service");
+const createArticlesBlockHandler = require("..//..//slack-ui/blocks/knowledgeArticlesBlockFormat");
 
 async function knowledgeArticlesSearch(searchTerm, channelId, username, userId, client) {
-    const userEmail = `${username}@salesforce.com`;
-    const results = await salesforceService.searchKnowledgeArticles(searchTerm, channelId, 10);
-    console.log(results);
-    
-    //TODO: Spreate to another file
-    //Creates blocks to display to the user
-    var articleBlocks = [];
-    results.forEach(article => {
-        articleBlocks.push({
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: article.Title,
-            },
-          });
-    });
+  const userEmail = `${username}@salesforce.com`;
+  const results = await salesforceService.searchKnowledgeArticles(searchTerm, channelId, 10);
+  console.log(results);
+
+  var url = await salesforceService.getDomain();
+  url = url + "/" + results.Id;
+
+  //Creates blocks to display to the user
+  var articleBlocks = [];
+  results.forEach(article => {
+    var link = url + "/" + results.Id;
+    var lastModifiedDate = getLastModifiedDateAsString(article.LastModifiedDate);
+    var block = createArticlesBlockHandler(article.Title, article.CreatedBy, lastModifiedDate, link);
+    articleBlocks.push(block);
+  });
 
   //Sends ephmeral message to the user
   await client.chat.postEphemeral({
@@ -27,6 +27,40 @@ async function knowledgeArticlesSearch(searchTerm, channelId, username, userId, 
     blocks: articleBlocks
   });
 
+}
+
+/**
+ * The function returns when was the business case last created/updated 
+ */
+function getLastModifiedDateAsString(createdDate, lastModifiedDate) {
+  var seconds = (new Date().getTime() - new Date(lastModifiedDate).getTime()) / 1000;
+  var retVal;
+  if (seconds > 31557600) {
+    var x = Math.floor(seconds / 31557600).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Years Ago' : x + ' Year Ago';
+  } else if (seconds > 2629800) {
+    var x = Math.floor(seconds / 2629800).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Months Ago' : x + ' Month Ago';
+  } else if (seconds > 604800) {
+    var x = Math.floor(seconds / 2629800).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Weeks Ago' : x + ' Week Ago';
+  } else if (seconds > 87660) {
+    var x = Math.floor(seconds / 87660).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Days Ago' : x + ' Day Ago';
+  } else if (seconds > 3600) {
+    var x = Math.floor(seconds / 3600).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Hours Ago' : x + ' Hour Ago';
+  } else {
+    var x = Math.floor(seconds / 60).toString();
+    if (x == 0) { x = 1; }
+    retVal = x >= 2 ? x + ' Minutes Ago' : x + ' Minute Ago';
+  }
+  return (createdDate == lastModifiedDate) ? 'Created ' + retVal : 'Updated ' + retVal;
 }
 
 module.exports = {
---
+++ slack-ui/blocks/knowledgeArticlesBlockFormat.js
@@ -0,0 +1,43 @@
+//The function receives information regarding a single knowledge article, creates a block for a search result and returns to the caller
+async function createArticlesMsgFormat(title, author, lastModified, link) {
+    var linkText = "<" + link + "|" + title + ">";
+    var contextTest = author + " • " + lastModified;
+    var blocks = [
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: "<www.google.com|The link titles will look like this Itai>"
+            },
+            accessory: {
+                type: "button",
+                text: {
+                    type: "plain_text",
+                    "text": "Open",
+                    emoji: true
+                },
+                value: "click_me_123",
+                url: "https://google.com",
+                action_id: "button-action"
+            }
+        },
+        {
+            type: "context",
+            elements: [
+                {
+                    type: "mrkdwn",
+                    text: contextTest
+                }
+            ]
+        },
+        {
+            type: "divider"
+        }
+    ]
+    return blocks;
+}
+
+
+module.exports = {
+    createArticlesMsgFormat,
+};
