fe52fd0f192e58011a4d6c77aac19587eb074523
Itai <ikatz@salesforce.com>
2022-11-16 12:46:59 +0200

    working on changes to caseCreation

--- slack-ui/blocks/createHcCetegoriesSelectionFormat.js
+++ slack-ui/blocks/createHcCetegoriesSelectionFormat.js
@@ -8,7 +8,7 @@ function createCategoriesSelectionFormat(privateMetadata, queryResult) {
     for (var x of queryResult) {
         if (x.Type__c == 'Picklist') {
             var opts = []
-            for (var j of x.Categories__r) {
+            for (var j of x.Categories__r.records) {
                 console.log(j);
                 opts.push({
                     text: {
